33a0675b376b056643f58796d78ae1d16f759316
Jonbv02 <135186142+Jonbv02@users.noreply.github.com>
2023-08-19 18:41:36 -0600

    Delete src directory

--- src/main/java/com/curso/curso/Repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.curso.curso.Repository;
-
-import com.curso.curso.Model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Task, Long> {
-}
